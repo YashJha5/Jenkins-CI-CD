eb7a87f89203d15f08343da549b10f35ebc06a74
jhayash55 <jhayash55@gmail.com>
2020-07-14 16:14:02 +0530

    Initial Commit

--- script-git.py
+++ script-git.py
@@ -2,13 +2,23 @@ from subprocess import check_output
 from datetime import date
 
 today = date.today()
-print("Today's date:", today)
+
+taareekh = today.strftime('%Y-%m-%d')
+print("Today's date:",taareekh)
 
 date_string = check_output('git log -1 --pretty=format:"%ci"').decode()
 
 
 #print(date_string)  // get the log of date
-data = date_string.split( )
+data = date_string.split()
+#print(data)
+#print(data[0])
 
 
-print(data[0])
+#print("Last Commited Date:",data[0])
+print(data[0]==taareekh)
+if (data[0] == taareekh):
+    print("Yusss Now we can build the setup")
+else: 
+    False
+
